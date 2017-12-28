45216c222a97421906557c738236078d4e49eb3d
fAtliNg <korgeflus@mail.ru>
2017-12-29 00:39:11 +0300

    deploy

--- ci/build_and_deploy.sh
+++ ci/build_and_deploy.sh
@@ -3,4 +3,5 @@ cd frontend
 npm install
 unset CI
 npm run build
+sshpass -p "$PASSWORD" ssh $USER@$HOST 'rm -r /var/www/nautilus/*'
 sshpass -p "$PASSWORD" rsync -rvz -e 'ssh -o StrictHostKeyChecking=no -p 22' --progress  build/* $USER@$HOST:/var/www/nautilus
