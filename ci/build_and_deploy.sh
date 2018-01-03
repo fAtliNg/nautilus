daf4c634d1cca0ddb4e408138c48146aaa911b64
fAtliNg <korgeflus@mail.ru>
2018-01-04 01:13:11 +0300

    test ci

--- ci/build_and_deploy.sh
+++ ci/build_and_deploy.sh
@@ -4,7 +4,7 @@ npm install
 unset CI
 npm run build
 cd ../backend
-maven install
+mvn install
 sshpass -p "$PASSWORD" ssh $USER@$HOST 'rm -r /var/www/nautilus/*'
 sshpass -p "$PASSWORD" rsync -rvz -e 'ssh -o StrictHostKeyChecking=no -p 22' --progress  build/* $USER@$HOST:/var/www/nautilus
 sshpass -p "$PASSWORD" ssh $USER@$HOST 'rm /var/lib/tomcat8/webapps/nautilus.war'
