cf130ff6d01f83c3e1e7567039c549a3147e9b72
fAtliNg <korgeflus@mail.ru>
2018-01-07 12:50:01 +0300

    Header

--- frontend/src/components/Navigation/Navigation.jsx
+++ frontend/src/components/Navigation/Navigation.jsx
@@ -30,6 +30,7 @@ class Navigation extends Component {
     render() {
         return (
             <div margin="10px">
+                <img style={{width: "100%"}} src="https://sun1-1.userapi.com/c639317/v639317161/5c8c5/ViE8kREWpkg.jpg"/>
                 <AppBar position="static">
                     <Toolbar>
                         {this.renderMenuItem('/news', 'Новости')}
