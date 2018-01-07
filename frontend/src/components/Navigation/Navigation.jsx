6e191d420c400d7f81967b266fd7e16e2ae1acb1
fAtliNg <korgeflus@mail.ru>
2018-01-07 22:07:58 +0300

    Navigation styled

--- frontend/src/components/Navigation/Navigation.jsx
+++ frontend/src/components/Navigation/Navigation.jsx
@@ -3,6 +3,14 @@ import AppBar from 'material-ui/AppBar';
 import Toolbar from 'material-ui/Toolbar';
 import Button from 'material-ui/Button';
 import {Link} from 'react-router-dom'
+import {withStyles} from 'material-ui/styles';
+
+const styles = {
+    toolbar: {
+        minHeight: 0,
+        backgroundColor: "#042554"
+    }
+};
 
 class Navigation extends Component {
     constructor(props) {
@@ -20,19 +28,23 @@ class Navigation extends Component {
 
     renderMenuItem = (url, label) => <Link to={url} style={{textDecoration: "none"}}>
         <Button
-            color={this.state.activeLink === url ? 'contrast' : 'default'}
+            color={"contrast"}
             onClick={() => {this.onChangePage(url)}}
         >
             {label}
         </Button>
+        {this.state.activeLink === url &&
+        <div style={{height: 4, backgroundColor: "#ffffff"}}/>
+        }
     </Link>;
 
     render() {
+        const {classes} = this.props;
         return (
-            <div margin="10px">
+            <div>
                 <img style={{width: "100%"}} src="https://sun1-1.userapi.com/c639317/v639317161/5c8c5/ViE8kREWpkg.jpg"/>
                 <AppBar position="static">
-                    <Toolbar>
+                    <Toolbar classes={{root: classes.toolbar}}>
                         {this.renderMenuItem('/news', 'Новости')}
                         {this.renderMenuItem('/players', 'Игроки')}
                         {this.renderMenuItem('/club', 'О клубе')}
@@ -45,4 +57,4 @@ class Navigation extends Component {
     }
 }
 
-export default Navigation;
+export default withStyles(styles)(Navigation);
