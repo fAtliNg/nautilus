2d3987cab388cfa4312cd4d51e6fbedcbdb7b724
fAtliNg <korgeflus@mail.ru>
2018-01-08 17:18:43 +0300

    ci

--- ci/build_and_deploy.sh
+++ ci/build_and_deploy.sh
@@ -7,8 +7,8 @@ npm install
 npm run build
 cd ../backend
 mvn install
-sshpass -p "$PASSWORD" ssh -o StrictHostKeychecking=no $USER@$HOST 'rm -r /var/www/nautilus/*'
-sshpass -p "$PASSWORD" rsync -rvz -e 'ssh -o StrictHostKeyChecking=no -p 22' --progress  ../frontend/build/* $USER@$HOST:/var/www/nautilus
+sshpass -p "$PASSWORD" ssh -o StrictHostKeychecking=no $USER@$HOST 'rm -r /var/www/mfc-nautilus/*'
+sshpass -p "$PASSWORD" rsync -rvz -e 'ssh -o StrictHostKeyChecking=no -p 22' --progress  ../frontend/build/* $USER@$HOST:/var/www/mfc-nautilus
 sshpass -p "$PASSWORD" ssh -o StrictHostKeychecking=no $USER@$HOST 'rm /var/lib/tomcat8/webapps/nautilus.war'
 sshpass -p "$PASSWORD" rsync -rvz -e 'ssh -o StrictHostKeyChecking=no -p 22' --progress  target/nautilus.war $USER@$HOST:/var/lib/tomcat8/webapps/
 sshpass -p "$PASSWORD" ssh -o StrictHostKeychecking=no $USER@$HOST 'service tomcat8 restart'
