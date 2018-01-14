e065bfc5f4799835c1369d8f7bff686b2c7e4b2b
fAtliNg <korgeflus@mail.ru>
2018-01-14 10:55:37 +0300

    Refactoring CardPlayer component

--- frontend/src/components/CardPlayer/CardPlayer.jsx
+++ frontend/src/components/CardPlayer/CardPlayer.jsx
@@ -3,51 +3,77 @@ import PropTypes from 'prop-types';
 import {withStyles} from 'material-ui/styles';
 import Card, {CardContent, CardMedia} from 'material-ui/Card';
 import Grid from 'material-ui/Grid';
-import Typography from 'material-ui/Typography';
-
-const styles = theme => ({
-    card: {
-        display: 'flex',
-    },
-    cover: {
-        height: "100%",
-    }
-});
+import styles from './styles';
 
 class CardPlayer extends Component {
     render() {
         const {classes, data} = this.props;
         return (
             <Card className={classes.card}>
-                <Grid container justify="center">
-                    <Grid item xs={5}>
-                        <CardMedia
-                            className={classes.cover}
-                            image={data.photo}
-                            title={data.fullName}
-                        />
+                <Grid container>
+                    <Grid item xs={12}>
+                        <Grid container direction={"row"}>
+                            <Grid item xs={4}>
+                                <img
+                                    className={classes.photo}
+                                    src={data.photo}
+                                />
+                            </Grid>
+                            <Grid item xs={8}>
+                                <Grid container direction="column">
+                                    <Grid item>
+                                        <div className={classes.number}>
+                                            <div className={classes.title}>
+                                                {data.number}
+                                            </div>
+                                        </div>
+                                    </Grid>
+                                    <Grid item style={{padding: 0, paddingLeft: 8}}>
+                                        <div className={classes.title}>
+                                            {data.fullName}
+                                        </div>
+                                    </Grid>
+                                </Grid>
+                            </Grid>
+                        </Grid>
                     </Grid>
-                    <Grid item xs={7}>
-                        <CardContent>
-                            <Typography type="headline">
-                                {data.fullName}
-                            </Typography>
-                            <Typography type="subheading" color="secondary">
-                                Возраст: {data.age}
-                            </Typography>
-                            <Typography type="subheading" color="secondary">
-                                Номер: {data.number}
-                            </Typography>
-                            <Typography type="subheading" color="secondary">
-                                Игровая позиция: {data.role}
-                            </Typography>
-                            <Typography type="subheading" color="secondary">
-                                Желтые карточки: {data.yellowCards}
-                            </Typography>
-                            <Typography type="subheading" color="secondary">
-                                Красные карточки: {data.redCards}
-                            </Typography>
-                        </CardContent>
+                    <Grid item xs={12} style={{paddingLeft: 16, paddingTop: 0}}>
+                        <Grid container direction={"row"} className={classes.info}>
+                            <Grid item xs={6} style={{padding: 0}}>
+                                <div className={classes.subheading}>
+                                    Номер: {data.number}
+                                </div>
+                            </Grid>
+                            <Grid item xs={6} style={{padding: 0}}>
+                                <div className={classes.subheading}>
+                                    Желтые: {data.yellowCards}
+                                </div>
+                            </Grid>
+                        </Grid>
+                        <Grid container direction={"row"} className={classes.info}>
+                            <Grid item xs={6} style={{padding: 0}}>
+                                <div className={classes.subheading}>
+                                    Красные: {data.redCards}
+                                </div>
+                            </Grid>
+                            <Grid item xs={6} style={{padding: 0}}>
+                                <div className={classes.subheading}>
+                                    Игр: 3
+                                </div>
+                            </Grid>
+                        </Grid>
+                        <Grid container direction={"row"} className={classes.info}>
+                            <Grid item xs={6} style={{padding: 0}}>
+                                <div className={classes.subheading}>
+                                    Голов: 5
+                                </div>
+                            </Grid>
+                            <Grid item xs={6} style={{padding: 0, paddingBottom: 8}}>
+                                <div className={classes.subheading}>
+                                    Передачи: 0
+                                </div>
+                            </Grid>
+                        </Grid>
                     </Grid>
                 </Grid>
             </Card>
---
+++ frontend/src/components/CardPlayer/styles.jsx
@@ -0,0 +1,30 @@
+export default function styles(theme) {
+    return {
+        card: {
+            display: 'flex',
+            backgroundColor: theme.palette.backgroundColorComponent,
+        },
+        photo: {
+            width: "100%"
+        },
+        number: {
+            backgroundColor: theme.palette.turquoise,
+            marginTop: 16,
+            textAlign: "center",
+            borderRadius: 4,
+            width: 30,
+            height: 25
+        },
+        info: {
+            paddingLeft: 8
+        },
+        infoItem: {
+            padding: 0
+        },
+        cover: {
+            height: "100%",
+        },
+        title: theme.font.title,
+        subheading: theme.font.subheading
+    }
+};
