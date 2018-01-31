0883db9d4689aa19691c7f17f7f887d6d27f2fc2
Denis Denisenko <denisenkodns@gmail.com>
2018-01-31 22:26:34 +0300

    Logging impl

--- backend/src/main/java/ru/nautilus/service/MonitoringService.java
+++ backend/src/main/java/ru/nautilus/service/MonitoringService.java
@@ -1,6 +1,8 @@
 package ru.nautilus.service;
 
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -19,6 +21,8 @@ import ru.nautilus.model.ScoresTableInfo;
 @Component
 public class MonitoringService {
 
+    private static final Logger log = LogManager.getLogger(MonitoringService.class);
+
     @Autowired
     private MongoTemplate mongoTemplate;
 
@@ -31,37 +35,53 @@ public class MonitoringService {
     @Scheduled(fixedDelay = 60 * 60 * 1000)//once per hour
     public void monitorGroupMembers(){
         try{
+            log.info("Monitoring vk group members start..");
+
             //TODO::make update mechanizm more smarter
             clearCollection(SubscribersInfo.class);
             mongoTemplate.insertAll(vkApi.getSubscribersList());
+
+            log.info("Monitoring vk group members finish successfull");
         } catch (Exception e) {
-            //TODO:: log exception info
+            log.error(e, e);
         }
     }
 
     @Scheduled(fixedDelay = 60 * 60 * 1000)//once per hour
     public void monitorGroupNews(){
         try{
+            log.info("Monitoring vk group news start..");
+
             //TODO::make update mechanizm more smarter
             clearCollection(NewsInfo.class);
             mongoTemplate.insertAll(vkApi.getNews());
+
+            log.info("Monitoring vk group news finish successfull");
         } catch (Exception e) {
-            //TODO:: log exception info
+            log.error(e, e);
         }
     }
 
     @Scheduled(fixedDelay = 60 * 60 * 1000)//once per hour
     public void monitorScoresTable(){
+        log.info("Monitoring score table start..");
+
         //TODO::make update mechanizm more smarter
         clearCollection(ScoresTableInfo.class);
         mongoTemplate.insertAll(goalstreamApi.getScoreTableInfo());
+
+        log.info("Monitoring score table finish successfull");
     }
 
     @Scheduled(fixedDelay = 60 * 60 * 1000)//once per hour
     public void monitorPlayersInfo(){
+        log.info("Monitoring players info start..");
+
         //TODO::make update mechanizm more smarter
         clearCollection(PlayersInfo.class);
         mongoTemplate.insertAll(goalstreamApi.getPlayersInfo());
+
+        log.info("Monitoring players finish successfull");
     }
 
     private void clearCollection(Class entityClass){
