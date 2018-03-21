b934c4423db98e42b8f18f348d30119f95cdc5e5
fAtliNg <korgeflus@mail.ru>
2018-03-21 11:59:03 +0300

    Update Achievements.jsx

--- frontend/src/components/AboutClub/Achievements/Achievements.jsx
+++ frontend/src/components/AboutClub/Achievements/Achievements.jsx
@@ -21,7 +21,7 @@ class Achievements extends React.Component {
                   <ListItem>
                     <div className={classes.subheading}>
                         {entry.url &&
-                            <a href={entry.url}>{entry.trophy}</a>
+                            <a className={classes.subheading} href={entry.url}>{entry.trophy}</a>
                         }
                         {!entry.url && entry.trophy}
                     </div>
