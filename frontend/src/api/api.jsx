b2f5738473be6267df40a6f16bf920c7a90c8d23
fAtliNg <korgeflus@mail.ru>
2018-03-21 11:48:19 +0300

    Update api.jsx
    пропущены запятые

--- frontend/src/api/api.jsx
+++ frontend/src/api/api.jsx
@@ -7,47 +7,28 @@ var axiosInstance = axios.create({
 })
 
 // Common
-export const fetchCommonScoresTable = () => {
-  return axiosInstance.get('scorestable')
-}
+export const fetchCommonScoresTable = () => axiosInstance.get('scorestable')
 
-export const fetchVkData = () => {
-  return axiosInstance.get('vk')
-}
+export const fetchVkData = () => axiosInstance.get('vk')
 
 // News Page
-export const fetchEntries = (limit, offset) => {
-  // return axiosInstance.get('news')
-  return axiosInstance.get(`news?limit=${limit}&offset=${offset}`)
-}
+export const fetchEntries = (limit, offset) => axiosInstance.get(`news?limit=${limit}&offset=${offset}`)
 
 // Players Page
-export const fetchPlayers = () => {
-  return axiosInstance.get('players')
-}
+export const fetchPlayers = () => axiosInstance.get('players')
 
 // Photo Page
-export const fetchPhotoAlbums = () => {
-  return axiosInstance.get('gallery/photo')
-}
+export const fetchPhotoAlbums = () => axiosInstance.get('gallery/photo')
 
-export const fetchPhotos = id => {
-  return axiosInstance.get(`gallery/photo/${id}`)
-}
+export const fetchPhotos = id => axiosInstance.get(`gallery/photo/${id}`)
 
 // Video Page
-export const fetchVideoAlbums = () => {
-  return axiosInstance.get('gallery/video')
-}
+export const fetchVideoAlbums = () => axiosInstance.get('gallery/video')
 
-export const fetchVideos = id => {
-  return axiosInstance.get(`gallery/video/${id}`)
-}
+export const fetchVideos = id => axiosInstance.get(`gallery/video/${id}`)
 
 // Events
-export const fetchEvents = () => {
-  return axiosInstance.get('events/getall')
-}
+export const fetchEvents = () => axiosInstance.get('events/getall')
 
 // About Club
 export const fetchAboutClubInfo = () => {
@@ -59,15 +40,15 @@ export const fetchAboutClubInfo = () => {
             'г. Ростов-ра-Дону',
       achievements: [
         {
-          trophy: 'Полуфиналист летнего корпоративного кубка 2017'
+          trophy: 'Полуфиналист летнего корпоративного кубка 2017',
           url: 'http://www.goalstream.org/season/1020574#/clubs'
         },
         {
-          trophy: '4 место  осень-2017 4я лига РОЛФЛ'
+          trophy: '4 место  осень-2017 4я лига РОЛФЛ',
           url: 'http://www.goalstream.org/season/1021553#/standings'
         },
         {
-          trophy: '3 место  зима-2017/18 5я лига РОЛФЛ'
+          trophy: '3 место  зима-2017/18 5я лига РОЛФЛ',
           url: 'http://www.goalstream.org/season/1022983/279a8cb8#/standings'
         }
       ]
