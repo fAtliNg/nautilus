f93b6440b7c4bec760244abf01dd18e78d6ab921
fAtliNg <korgeflus@mail.ru>
2018-03-21 14:44:52 +0300

    Update AboutInfo.jsx

--- frontend/src/components/AboutClub/AboutInfo/AboutInfo.jsx
+++ frontend/src/components/AboutClub/AboutInfo/AboutInfo.jsx
@@ -1,8 +1,5 @@
 import React from 'react'
 import Paper from 'material-ui/Paper'
-import List, {
-    ListItem
-} from 'material-ui/List'
 import { withStyles } from 'material-ui/styles'
 import styles from './styles'
 
@@ -12,16 +9,6 @@ class AboutInfo extends React.Component {
     return (
       <Paper className={classes.textPaper}>
         {text.length && text.split('<br>').map(t => <div className={classes.subheading}>{t}</div>)}
-        <div className={classes.title}>
-          Контакты
-        </div>
-        <List>
-            <ListItem>
-                <div className={classes.subheading}>
-                    Электронная почта: denisenkodns@gmail.com
-                </div>
-            </ListItem>
-        </List>
       </Paper>
     )
   }
